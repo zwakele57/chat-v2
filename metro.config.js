529f0a0ea33d9beb65180fad10071db5c9f0dfbe
attacktitan123 <attacktitanishere@gmail.com>
2025-07-15 01:18:15 +0200

    Create New Expo React Native Chat App

--- metro.config.js
+++ metro.config.js
@@ -1,12 +1,5 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
 
 const config = getDefaultConfig(__dirname);
-
-// Add explicit node modules path for expo-modules-core
-config.resolver.nodeModulesPaths = [
-  path.resolve(__dirname, 'node_modules/expo-modules-core'),
-  ...config.resolver.nodeModulesPaths || []
-];
 
 module.exports = config;
