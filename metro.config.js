28642f72280e98ba1cd3ebc4e478e9ed33cda46c
attacktitan123 <attacktitanishere@gmail.com>
2025-07-15 01:03:27 +0200

    Fix Metro config for TypeScript transpilation

--- metro.config.js
+++ metro.config.js
@@ -2,7 +2,7 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Add TypeScript extensions to resolver
-config.resolver.sourceExts.push('ts', 'tsx');
+// Ensure proper Babel transformation for TypeScript files
+config.transformer.babelTransformerPath = require.resolve('metro-react-native-babel-transformer');
 
 module.exports = config;
