683d0c885cfa8044d3527c3a6d2f3ba6f3824a0e
attacktitan123 <attacktitanishere@gmail.com>
2025-07-15 01:15:04 +0200

    Fix Metro config for TypeScript resolution

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,8 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Enable symlinks and proper module resolution for TypeScript files
+config.resolver.unstable_enableSymlinks = true;
+config.resolver.unstable_conditionNames = ['browser', 'module', 'main'];
+
 module.exports = config;
